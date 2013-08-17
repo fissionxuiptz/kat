501120d147a4d1e0ccf1425f3c76074f339a2061
Fission Xuiptz <fissionxuiptz@softwaremojo.com>
2013-08-17 17:54:44 +1000

    Tidied up the App class a little more

--- lib/kat/app.rb
+++ lib/kat/app.rb
@@ -32,71 +32,79 @@ module Kat
     end
 
     def init_search
-      @kat = Kat.search @args.join(' '), @options
+      @kat ||= Kat.search
+      @kat.query = @args.join(' ')
+      @kat.options = @options
     end
 
     def main
       puts VERSION_STR
 
-      unless (list_args = Kat::Search.selects.select {|k, v| @options[v[:select]] }).empty?
-        list_args.each do |k, v|
-          list_args = Kat.send v[:select]
-          puts "\n  #{v[:select].to_s.capitalize}"
-          puts unless Array === list_args.values.first
-          list_args.each {|k, v| puts Array === v ? "\n  %12s => #{v.join "\n\t\t  "}" % k : "  %-23s => #{v}" % k }
-          puts
+      Kat::Search.selects.select {|k, v| @options[v[:select]] }.tap do |lists|
+        unless lists.empty?
+          list_args.each do |k, v|
+            list_args = Kat.send v[:select]
+            puts "\n  #{v[:select].to_s.capitalize}"
+            puts unless Array === list_args.values.first
+            list_args.each {|k, v| puts Array === v ? "\n  %12s => #{v.join "\n\t\t  "}" % k : "  %-23s => #{v}" % k }
+            puts
+          end
+        else
+          while running; end
         end
+      end
+    end
+
+  private
+
+    def running
+      r = @kat.search page
+      if r.nil?
+        puts "\nNo results"
+        return nil
+      end
+
+      n = @page < @kat.pages - 1
+      p = @page > 0
+
+      puts "\n%-72s S     L\n\n" % "Page #{page + 1} of #{@kat.pages}"
+      r.each_with_index do |t, i|
+        puts "%2d. %-64s %5d %5d" % [ i + 1, t[:title][0..63], t[:seeds], t[:leeches] ]
+      end
+
+      commands = "[#{'n' if n}#{'p' if p}q]|"
+      _01to09  = "[1#{r.size >  9 ? '-9' : '-' + r.size.to_s}]"
+      _10to19  = "#{r.size   >  9 ? '|1[0-' + (r.size > 19 ? '9' : (r.size - 10).to_s) + ']' : ''}"
+      _20to25  = "#{r.size   > 19 ? '|2[0-' + (r.size - 20).to_s + ']' : ''}"
+      prompt   = "\n1#{r.size > 1 ? '-' + r.size.to_s : ''} to download" +
+                 "#{', (n)ext' if n}" +
+                 "#{', (p)rev' if p}" +
+                 ', (q)uit: '
+
+      case (answer = @h.ask(prompt) {|q| q.validate = /^(#{commands}#{_01to09}#{_10to19}#{_20to25})$/ })
+      when 'q' then return nil
+      when 'n' then @page += 1 if n
+      when 'p' then @page -= 1 if p
       else
-        loop do
-          r = @kat.search page
-          if r.nil?
-            puts "\nNo results"
-            break
+        if (1..r.size).include? answer.to_i
+          torrent = @kat.results[page][answer.to_i - 1]
+          puts "\nDownloading: #{torrent[:title]}"
+
+          begin
+            uri = URI torrent[:download]
+            uri.query = nil
+            response = uri.read
+            file = "#{File.expand_path(options[:output] || '.')}/#{torrent[:title].gsub(/ /, '.').gsub(/[^a-z0-9()_.-]/i, '')}.torrent"
+            File.open(file, 'w') {|f| f.write response }
+          rescue => e
+            puts e.message
           end
+        end
+      end
 
-          n = @page < @kat.pages - 1
-          p = @page > 0
+      return 1
+    end
 
-          puts "\n%-72s S     L\n\n" % "Page #{page + 1} of #{@kat.pages}"
-          r.each_with_index do |t, i|
-            puts "%2d. %-64s %5d %5d" % [ i + 1, t[:title][0..63], t[:seeds], t[:leeches] ]
-          end
+  end
 
-          commands = "[#{'n' if n}#{'p' if p}q]|"
-          _01to09  = "[1#{r.size >  9 ? '-9' : '-' + r.size.to_s}]"
-          _10to19  = "#{r.size   >  9 ? '|1[0-' + (r.size > 19 ? '9' : (r.size - 10).to_s) + ']' : ''}"
-          _20to25  = "#{r.size   > 19 ? '|2[0-' + (r.size - 20).to_s + ']' : ''}"
-          prompt   = "\n1#{r.size > 1 ? '-' + r.size.to_s : ''} to download" +
-                     "#{', (n)ext' if n}" +
-                     "#{', (p)rev' if p}" +
-                     ', (q)uit: '
-
-          case (answer = @h.ask(prompt) {|q| q.validate = /^(#{commands}#{_01to09}#{_10to19}#{_20to25})$/ })
-          when 'q' then break
-          when 'n' then @page += 1 if n
-          when 'p' then @page -= 1 if p
-          else
-            if (1..r.size).include? answer.to_i
-              torrent = @kat.results[page][answer.to_i - 1]
-              puts "\nDownloading: #{torrent[:title]}"
-
-              begin
-                uri = URI torrent[:download]
-                uri.query = nil
-                response = uri.read
-                file = "#{File.expand_path(options[:output] || '.')}/#{torrent[:title].gsub(/ /, '.').gsub(/[^a-z0-9()_.-]/i, '')}.torrent"
-                File.open(file, 'w') {|f| f.write response }
-              rescue => e
-                puts e.message
-              end
-            end
-          end # case
-        end # loop
-
-        puts
-      end # unless
-    end # main
-
-  end # App
-
-end # Kat
+end
