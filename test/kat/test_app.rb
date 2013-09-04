060bf07a86c655bb6461fcae2d07154e224a9e15
Fission Xuiptz <fissionxuiptz@softwaremojo.com>
2013-09-04 13:19:19 +1000

    Fixed a couple of app tests

--- test/kat/test_app.rb
+++ test/kat/test_app.rb
@@ -5,8 +5,8 @@ describe Kat::App do
   describe 'app' do
     it '' do
       app = Kat::App.new %w(predator -c movies)
-      app.kat.must_be_instance_of Kat
-      app.kat.options.must_be_instance_of Array
+      app.kat.must_be_instance_of Kat::Search
+      app.kat.options.must_be_instance_of Hash
       #app.kat.options
     end
   end
