23e8e146dc04df6ef964b32ddacba119c523a941
Fission Xuiptz <fissionxuiptz@softwaremojo.com>
2013-09-18 22:54:25 +1000

    v2.0.0

--- lib/kat/version.rb
+++ lib/kat/version.rb
@@ -1,6 +1,6 @@
 module Kat
   NAME = 'Kickass Torrents Search'
-  VERSION = '2.0.0beta'
+  VERSION = '2.0.0'
   MALEVOLENT_DICTATOR_FOR_LIFE = 'Fission Xuiptz'
   AUTHOR = MALEVOLENT_DICTATOR_FOR_LIFE
   VERSION_STR = "#{NAME} #{VERSION} (c) 2013 #{MALEVOLENT_DICTATOR_FOR_LIFE}"
